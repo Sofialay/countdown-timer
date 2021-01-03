8e860bfca11b15b404ca1996b4f8d5f702d5e72f
Sofía Lay <sofi-lay@hotmail.com>
2021-01-03 00:29:37 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import CardContainer from './components/CardContainer/CardContainer';
+import mountain from '././images/pattern-hills.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="contdown-page">
+      {/* falta hacer el diseño */}
+      <h3 className="title">Sofía Lay cumpleañito</h3> 
+      <CardContainer/>
+      <img className="mountain-bg" src={mountain} alt="mountain illustration"/>
+    </section>
   );
 }
 
